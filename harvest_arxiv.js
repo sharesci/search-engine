c375882e77c3cecf4cb4c3a1642e99d1c1340960
Mike D'Arcy <m.darcy.220@gmail.com>
2017-04-16 22:58:23 -0400

    Add batch capability to arXiv harvester

--- harvest_arxiv.js
+++ harvest_arxiv.js
@@ -6,48 +6,87 @@ var request = require('request'),
 	xml2js = require('xml2js');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/sharesci';
+var mongo_url = 'mongodb://localhost:27017/sharesci';
+
+var first_url = 'http://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=arXiv&from=2017-04-11';
+var resume_url = 'http://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=';
 
 
-function mongoInsertPaperCallback(db, paperdata, callback) {
-	var collection = db.collection('papers');
-	collection.drop();
-	collection.insertMany(paperdata, (err, result) => {
-		assert.equal(err, null);
-		console.log("Inserted " + result.result.n + " papers");
-		callback(result);
+function oaiXmlToJson(oaiXML) {
+	alldata = [];
+	var metadatax = oaiXML('record > metadata > arXiv');
+	oaiXML('categories', metadatax).remove();
+	metadatax.each((index, obj) => {
+		xml2js.parseString(oaiXML.xml(obj), {explicitArray: false, trim: true}, (err, data) => {
+			data = data['arXiv'];
+			delete data['$'];
+			data['arXiv_id'] = data['id'];
+			delete data['id'];
+			data['authors'] = data['authors']['author'];
+			data['references'] = [];
+			alldata.push(data);
+		});
+	});
+	return alldata;
+}
+
+function mongoClearPapers(callback) {
+	MongoClient.connect(mongo_url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to server for clearing papers");
+		var collection = db.collection('papers');
+		collection.drop();
+		db.close();
+		callback(0);
 	});
 }
 
-function mongoInsertPaper(paperdata) {
-		MongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
-			console.log("Connected successfully to server");
-			mongoInsertPaperCallback(db, paperdata, (result)=>{db.close();});
+function mongoInsertPapers(paperdata, callback) {
+	MongoClient.connect(mongo_url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to server for inserting papers");
+		var collection = db.collection('papers');
+		collection.insertMany(paperdata, (err, result) => {
+			assert.equal(err, null);
+			console.log("Inserted " + result.result.n + " papers");
+			db.close();
+			callback(result);
 		});
+	});
+}
+
+function harvestOAI(url, last_promise) {
+	var reqpromise = new Promise((resolve, reject) => {
+		request(url, (err,res,xml)=>{resolve([err, res, xml]);});
+	});
+	reqpromise.then((vals) => {
+		var error = vals[0],
+			response = vals[1],
+			xml = vals[2];
+		
+		if (error || response.statusCode != 200) {
+			console.error(error, '\nstatusCode = ' + response.statusCode);
+			return;
+		}
+		var xmld = cheerio.load(xml, {
+			xmlMode: true
+		});
+		var resumptionToken = xmld('ListRecords > resumptionToken').text();
+		console.log('resumption = ' + resumptionToken);
+		console.log('resumptionXML = ' + xmld.html('ListRecords > resumptionToken'));
+		var alldata = oaiXmlToJson(xmld);
+
+		var mongoPromise = new Promise((resolve, reject) => {
+			last_promise.then(() => {
+				mongoInsertPapers(alldata, resolve);
+			});
+		});
+		if (resumptionToken && 0 < resumptionToken.length) {
+			setTimeout(()=>{harvestOAI(resume_url + resumptionToken, mongoPromise);}, 30000);
+		}
+	});
 }
 
 
-request('http://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=arXiv&from=2017-04-01', function(error, response, html) {
-	if (!error && response.statusCode == 200) {
-		var xmld = cheerio.load(html, {
-			xmlMode: true
-		});
-		alldata = [];
-		var metadatax = xmld('record > metadata > arXiv');
-		xmld('categories', metadatax).remove();
-		metadatax.each((index, obj) => {
-			xml2js.parseString(xmld.xml(obj), {explicitArray: false, trim: true}, (err, data) => {
-				data = data['arXiv'];
-				delete data['$'];
-				data['arXiv_id'] = data['id'];
-				delete data['id'];
-				data['authors'] = data['authors']['author'];
-				data['references'] = [];
-				alldata.push(data);
-			});
-		});
-		mongoInsertPaper(alldata);
-	}
-});
+(new Promise((resolve,reject)=>{mongoClearPapers(resolve);})).then((val)=>{harvestOAI(first_url, Promise.resolve(0));});
 
