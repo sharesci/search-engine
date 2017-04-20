25cc57c29bb435f91b403fa9b53fabbe8df36aa4
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-04-20 00:44:57 +0000

    Add basic login backend

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-var router = require('./config/routes.js');
+const
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	express_session = require('express-session'),
+	https = require('https'),
+	http = require('http'),
+	fs = require('fs'),
+	pg = require('pg-promise')(),
+	bcrypt = require('bcrypt'),
+	router = require('./config/routes.js');
 
-var app = express();
+const app = express();
+const pgclient = pg({
+	'host': 'localhost',
+	'port': 5432,
+	'user': 'sharesci',
+	'password': 'sharesci',
+	'database': 'sharesci',
+});
 
 var https_ok = true;
 var https_options = {};
@@ -16,16 +27,110 @@ try {
 	};
 } catch (err) {
 	https_ok = false;
-	console.error(err);
+	if (err.errno === -13 && err.syscall === 'open') {
+		console.error('Access permissions denied to SSL certificate files.' +
+			' HTTPS will not be available. Try running as root.');
+	} else {
+		console.error(err);
+	}
 }
 
 app.use('/', express.static(__dirname + '/static'));
+app.use(express_session({
+	secret: require('crypto').randomBytes(64).toString('base64'),
+	resave: false,
+	saveUninitialized: false,
+	httpOnly: true,
+	secure: true,
+	ephemeral: true,
+	cookie: { maxAge: 60*1000 }
+}));
+
+
+app.use('/login', bodyParser.urlencoded({ extended: true }));
+app.post('/login', (req, res) => {
+	if(req.session.user_id) {
+		if (req.body.successRedirect) {
+			res.redirect(req.body.successRedirect);
+		} else {
+			res.redirect('/');
+		}
+	}
+	const query = 'SELECT passhash FROM account WHERE username = ${username};';
+	var values = {
+		'username': req.body.username
+	};
+	pgclient.one(query, values)
+		.then((data) => {
+			if (bcrypt.compareSync(req.body.password, data['passhash'])) {
+				req.session.user_id = req.body.username;
+				if (req.body.successRedirect) {
+					res.redirect(req.body.successRedirect);
+				} else {
+					res.redirect('/');
+				}
+			} else if (req.body.failureRedirect) {
+				res.redirect(req.body.failureRedirect);
+			} else {
+				res.redirect('/');
+			}
+			res.end();
+		})
+		.catch((err) => {
+			if(err.received === 0) {
+				console.log('Invalid username \'' + req.body.username + '\' tried to log in.');
+			} else {
+				console.error(err);
+			}
+			if (req.body.failureRedirect) {
+				res.redirect(req.body.failureRedirect);
+			} else {
+				res.redirect('/');
+			}
+			res.end();
+		});
+});
+
+app.use('/logout', bodyParser.urlencoded({ extended: true }));
+app.use('/logout', (req, res) => {
+	delete req.session['user_id'];
+	req.session.destroy();
+	if (req.body.successRedirect) {
+		res.redirect(req.body.successRedirect);
+	} else {
+		res.redirect('/');
+	}
+});
+
+app.use('/new', bodyParser.urlencoded({ extended: true }));
+app.use('/new', (req, res) => {
+	var passsalt = bcrypt.genSaltSync(10);
+	console.log(passsalt);
+	var passhash = bcrypt.hashSync(req.body.password, passsalt);
+	const query = 'INSERT INTO account (username, passhash) VALUES (${username}, ${passhash});';
+	var values = {
+		'username': req.body.username,
+		'passhash': passhash,
+	};
+	pgclient.any(query, values)
+		.then((data) => {
+			res.redirect('/');
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+});
+
+function check_login(user, pass, callback) {
+}
+
+
 app.use('/', router);
 app.set('views', __dirname + '/src/views');
 app.set('view engine', 'ejs');
 
-http.createServer(app).listen(7080);
+http.createServer(app).listen(9080);
 
 if (https_ok) {
-	https.createServer(https_options, app).listen(7443);
+	https.createServer(https_options, app).listen(9443);
 }
