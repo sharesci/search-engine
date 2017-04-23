d4bf7ef7e256b8357f968b641abb01a20bd18868
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-04-23 00:12:45 +0000

    Add GET article to API
     GET requests to `/api/v1/article` return article data in JSON

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,11 +1,12 @@
 const
 	express = require('express'),
 	searchController = require('../../../controllers/api/v1/search');
+	articleController = require('../../../controllers/api/v1/article');
 
 var router = express.Router();
 
-router.get('/search', require('body-parser').urlencoded({extended:true}));
 router.get('/search', searchController.index);
+router.get('/article', articleController.getArticle);
 
 module.exports = router;
 
