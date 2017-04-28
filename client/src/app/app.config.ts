288200cd0ec82a0d5497e79630c92806fd5b5443
Utkarsh Patel <utkarshjp7@Gmail.com>
2017-04-28 01:34:06 -0400

    Dynamically find base url

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,3 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+
+@Component({
+    providers: [Location]
+})
+
 export class AppConfig {
-    public readonly apiUrl = 'http://127.0.0.1:7080';
+    constructor(@Inject(DOCUMENT) private document: any) { 
+        this.apiPort = 7080;
+        this.apiUrl = 'http://' + this.document.location.hostname + ':' + this.apiPort;
+    }
+
+    public readonly apiUrl : string;
+    public readonly apiPort : number;
 };
