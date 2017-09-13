d163442ca421881bf6536edf6fa97ad88f3c4a80
Perl Dev 1 <m.m.darcy@vikes.csuohio.edu>
2017-09-12 21:11:27 -0400

    Add min freq threshold to TextTrainingData

--- word2vec/TextTrainingData.py
+++ word2vec/TextTrainingData.py
@@ -6,23 +6,75 @@ import numpy as np
 
 
 class TextTrainingData:
-	def __init__(self):
+	def __init__(self, min_word_freq=2):
+		self._min_word_freq = min_word_freq
 		self.token2id = {}
 		self.id2freq = []
 		self.text_as_id_list = []
 		self.token_regex = re.compile(r"(?u)\b\w+\b")
 
+		# We'll use this to keep track of positions of infrequent
+		# tokens, without having to clutter the real token2id table
+		# with them
+		self._infrequent_tokens = {}
+
 
 	def add_text(self, text):
 		token_list = self.token_regex.findall(text)
 		for token in token_list:
-			token_id = 0
-			if not (token in self.token2id):
-				token_id = len(self.id2freq)
-				self.token2id[token] = token_id
-				self.id2freq.append(0)
-			else:
-				token_id = self.token2id[token]
+			self._add_token(token)
 
+
+	def _add_token(self, token):
+		if token in self.token2id:
+			token_id = self.token2id[token]
 			self.id2freq[token_id] += 1
 			self.text_as_id_list.append(token_id)
+			return
+
+		if token not in self._infrequent_tokens:
+			self._infrequent_tokens[token] = []
+
+		self._infrequent_tokens[token].append(len(self.text_as_id_list))
+		self.text_as_id_list.append(-1)
+
+		# Once we have enough appearances, make this a real token
+		if len(self._infrequent_tokens[token]) == self._min_word_freq:
+			token_id = len(self.id2freq)
+			self.token2id[token] = token_id
+			self.id2freq.append(len(self._infrequent_tokens[token]))
+
+			# Fill in the placeholders with the real id
+			for index in self._infrequent_tokens[token]:
+				self.text_as_id_list[index] = token_id
+
+			self._infrequent_tokens.pop(token, None)
+
+
+
+	## Get rid of all references to infrequent tokens
+	# 
+	# This includes both removing them from the word list and clearing the
+	# temporary infrequent token dict
+	#
+	def purge_infrequent_tokens(self):
+		all_indexes = []
+		for k in self._infrequent_tokens:
+			all_indexes.extend(self._infrequent_tokens[k])
+
+		all_indexes = sorted(all_indexes)
+
+		new_text_id_list = []
+		for i in range(len(all_indexes)):
+			if i < len(all_indexes)-1:
+				new_text_id_list.extend(self.text_as_id_list[all_indexes[i]+1:all_indexes[i+1]])
+			else:
+				new_text_id_list.extend(self.text_as_id_list[all_indexes[i]+1:])
+			#del self.text_as_id_list[index]
+
+		self.text_as_id_list = new_text_id_list
+
+		num_infrequent = len(self._infrequent_tokens.keys())
+		self._infrequent_tokens = {}
+
+		return len(all_indexes), num_infrequent
--- word2vec/save_TextTrainingData.py
+++ word2vec/save_TextTrainingData.py
@@ -3,13 +3,40 @@
 import pickle
 from TextTrainingData import TextTrainingData
 import json
+import os
 
-with open('../cranfield_data/cran.json', 'r') as f:
-	cran_data = json.load(f)
+datasource = 'arxiv'
 
-data = TextTrainingData()
-for doc in cran_data:
-	data.add_text(doc['W'])
+
+data = TextTrainingData(min_word_freq=6)
+
+
+if datasource == 'cranfield':
+	with open('../cranfield_data/cran.json', 'r') as f:
+		cran_data = json.load(f)
+
+	for doc in cran_data:
+		data.add_text(doc['W'])
+	
+elif datasource == 'arxiv':
+	allfiles = []
+	for root, dirs, files in os.walk('/mnt/data_partition/sharesci/arxiv/preproc/tmp/'):
+		allfiles.extend([root+name for name in files])
+
+	i = 0
+	for filename in allfiles:
+		with open(filename, 'r') as f:
+			data.add_text(f.read())
+		if i % 1000 == 0:
+			print(i)
+		i += 1
+
+	print('Deleting infrequent tokens')
+	num_positions_deleted, num_tokens_deleted = data.purge_infrequent_tokens()
+	print('Deleted {:d} tokens and {:d} positions'.format(num_tokens_deleted, num_positions_deleted))
+	print('Total vocab size in the end is {:d}'.format(len(data.id2freq)))
+	print('Total text size in the end is {:d}'.format(len(data.text_as_id_list)))
+
 
 with open('tmp_textdata.pickle', 'wb') as f:
 	pickle.dump(data, f)
