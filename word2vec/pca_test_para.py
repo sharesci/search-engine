640bd00685a7d19a3d04d43b157d40dfd73ceab8
Perl Dev 1 <m.m.darcy@vikes.csuohio.edu>
2017-10-06 12:56:56 -0400

    Add PCA projections

---
+++ word2vec/pca_test_para.py
@@ -0,0 +1,48 @@
+import os
+import pickle
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from cntk.ops.functions import load_model
+
+test_words = ['http', 'html', 'neural', 'algorithm', 'math', 'science']
+
+def get_model_weights():
+	#z = load_model(os.path.join(os.getcwd(), "word2vec_checkpoint"))
+	#weights = z.E.value
+	with open('../largedata/paragraph_vectors.npy', 'rb') as f:
+		weights = np.load(f)
+	return weights
+
+def reduce_dimentions(weights):
+	reduced_weights = PCA(n_components = 2).fit_transform(weights)
+	return reduced_weights
+
+def plot_graph(words, all_points, token2id):
+	points = np.empty([len(words), 2])
+	points = all_points[:10]
+#	for idx, word in enumerate(words):
+#		word_id = token2id[word]
+#		points[idx] = all_points[word_id]
+
+	#Plot the coordinates of test words
+	plt.scatter(points[:,0], points[:,1])
+
+	#Annotate the points
+	i = 0
+	for row in points:
+		plt.annotate(words[i+1], xy=row)
+		i += 1
+
+	plt.show()
+
+weights = get_model_weights()
+all_points = reduce_dimentions(weights)
+
+training_data = pickle.load(open('../largedata/text_training_data.pickle', 'rb'))
+inverse_dict = {v: k for k,v in training_data.doc2id.items()}
+
+plot_graph(inverse_dict, all_points, training_data.doc2id)
+
+
+
