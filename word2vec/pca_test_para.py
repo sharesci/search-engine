429ce2f90c077bfc69ddcc8db196a3527225ad12
Perl Dev 1 <m.m.darcy@vikes.csuohio.edu>
2017-10-21 03:38:23 -0400

    Add self-documentation to pca_test_para.py
       - Make variable names more intuitive
       - Add command examples in comments at the top of the file

--- word2vec/pca_test_para.py
+++ word2vec/pca_test_para.py
@@ -1,3 +1,40 @@
+#!/usr/bin/python3
+
+## @file
+#
+# This script allows visualizing high-dimensional vectors with a 2D projection
+#
+# The part warranting the most explanation here is the names_file, which
+# specifies which items are to be projected and plotted. It should be a JSON
+# file with a structure as follows:
+#
+# {
+#     "group1_label": [
+#         "name1",
+#         "name2", 
+#         ...,
+#     ],
+#     "group2_label": [
+#         "name1",
+#         "name2", 
+#         ...,
+#     ],
+#     ...,
+# }
+#
+# In essence, the file should contain an object with each key being the label
+# for a group of points to be plotted, and the value for each key is an array
+# of the names of the items to be plotted.
+#
+# EXAMPLES
+#
+# Visualizing word vectors from word2vec (file names may be different for you)
+#     python3 this_file.py my_words.json --legend 'upper left' --weights_file ../largedata/word2vec_vectors.npy --name2id_file ../largedata/token2id.json --annotate_points
+#
+# Visualizing document vectors:
+#     python3 this_file.py my_doc_names.json --legend 'upper left' -- weights_file ../largedata/paragraph_vectors.npy --name2id_file ../largedata/doc2id.json
+#
+
 import os
 import pickle
 import json
@@ -12,22 +49,11 @@ import scipy.sparse
 from argparse import ArgumentParser
 
 
-
-def get_model_weights():
-	with open('../largedata/paragraph_vectors.npy', 'rb') as f:
-		weights = np.load(f)
-	return weights
-
-
-def reduce_dimentions(weights):
-	return TruncatedSVD(n_components = 2).fit_transform(weights)
-
-
-def plot_graph(doc_names, all_points, doc2id, label='', annotate=False):
-	points = np.zeros([len(doc_names), 2])
-	for idx, doc in enumerate(doc_names):
-		doc_id = doc2id[doc]
-		points[idx] = all_points[doc_id]
+def plot_graph(names, projected_vectors, name2id, label='', annotate=False):
+	points = np.zeros([len(names), 2])
+	for idx, name in enumerate(names):
+		vector_id = name2id[name]
+		points[idx] = projected_vectors[vector_id]
 
 	#Plot the coordinates of test words
 	plt.scatter(points[:,0], points[:,1], label=label)
@@ -36,7 +62,7 @@ def plot_graph(doc_names, all_points, doc2id, label='', annotate=False):
 	if annotate:
 		i = 0
 		for row in points:
-			plt.annotate(doc_names[i], xy=row)
+			plt.annotate(names[i], xy=row)
 			i += 1
 
 
@@ -44,23 +70,25 @@ if __name__ == '__main__':
 	parser = ArgumentParser()
 	parser.add_argument('--annotate_points', dest='annotate_points', help='Annotate the data points', action='store_true', default=False)
 	parser.add_argument('--legend', dest='legend', help='Where to put the legend (compatible with pyplot syntax, "none" to have no legend)', action='store', default='none')
-	parser.add_argument('--doc2id_file', dest='doc2id_file', help='File where the doc2id JSON is located', action='store', default='../largedata/doc2id.json')
-	parser.add_argument('names_file', help='File from which to get the doc names to test, in JSON format', action='store')
+	parser.add_argument('--name2id_file', dest='name2id_file', help='File where the name2id JSON is located (e.g., doc2id.json, token2id.json, etc)', action='store', default='../largedata/doc2id.json')
+	parser.add_argument('--weights_file', dest='weights_file', help='File where the vector matrix is stored (in Numpy format)', action='store', default='../largedata/paragraph_vectors.npy')
+	parser.add_argument('names_file', help='File from which to get the names to test, in JSON format', action='store')
 	cmdargs = parser.parse_args(sys.argv[1:])
 
-	weights = get_model_weights()
-	all_points = reduce_dimentions(weights)
+	with open(cmdargs.weights_file, 'rb') as f:
+		weights = np.load(f)
+	projected_vectors = TruncatedSVD(n_components = 2).fit_transform(weights)
 
 	with open(cmdargs.names_file, 'r') as f:
-		test_doc_names = json.load(f)
+		names_to_plot = json.load(f)
 
-	with open(cmdargs.doc2id_file, 'r') as f:
-		doc2id = json.load(f)
+	with open(cmdargs.name2id_file, 'r') as f:
+		name2id = json.load(f)
 
 	fig = plt.figure(figsize=(6.5,5.5))
 
-	for k in sorted(test_doc_names):
-		plot_graph(test_doc_names[k], all_points, doc2id, label=k, annotate=cmdargs.annotate_points)
+	for k in sorted(names_to_plot):
+		plot_graph(names_to_plot[k], projected_vectors, name2id, label=k, annotate=cmdargs.annotate_points)
 
 	if cmdargs.legend != 'none':
 		plt.legend(loc=cmdargs.legend)
