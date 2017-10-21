6266576ee85df7ade02626bd1b4a817d1639cfb6
Perl Dev 1 <m.m.darcy@vikes.csuohio.edu>
2017-10-21 03:43:27 -0400

    Misc changes to vectorize_doc.py

--- word2vec/vectorize_doc.py
+++ word2vec/vectorize_doc.py
@@ -1,15 +1,19 @@
+#!/usr/bin/python3
+
 import numpy as np
 import pickle
 import os
 import re
 import json
 import sys
+import scipy.sparse
 from NumpyEmbeddingStorage import NumpyEmbeddingStorage
 from QueryEngineCore import QueryEngineCore
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('--vectorizer_type', dest='vectorizer_type', action='store', type=str, default='word2vec')
+parser.add_argument('--no-tsv-output', dest='tsv_output', action='store_false',  default=True)
 cmdargs = parser.parse_args(sys.argv[1:])
 
 datasource = 'cranfield'
@@ -18,6 +22,9 @@ data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'large
 
 with open(os.path.join(data_dir, 'token2id.pickle'), 'rb') as f:
 	token2id = pickle.load(f);
+
+with open(os.path.join(data_dir, 'id2freq.npy'), 'rb') as f:
+	id2freq = np.load(f);
 
 with open(os.path.join(data_dir, 'doc2id.json'), 'r') as f:
 	doc2id = json.load(f);
@@ -51,6 +58,36 @@ class Word2vecDocVectorizer:
 		unitvec_docs = text_embeddings_arr/docs_norm
 
 		return NumpyEmbeddingStorage(unitvec_docs)
+
+
+class TfIdfDocVectorizer:
+	def __init__(self, vocab_size):
+		self._vocab_size = vocab_size
+		self._token_regex = re.compile(r"(?u)\b\w+\b")
+
+
+	def make_doc_vector(self, text):
+		vec = np.zeros(self._vocab_size)
+		for token in self._token_regex.findall(text):
+			if token in token2id:
+				vec[token2id[token]] += 1
+		return np.log(vec+1)
+
+
+	def make_doc_embedding_storage(self, texts):
+		text_embeddings = []
+		i = 0
+		text_embeddings_arr = np.zeros((len(texts), self._vocab_size))
+		for text in texts:
+			#print(i)
+			i += 1
+			text_embeddings_arr[i-1] = self.make_doc_vector(text)
+
+		docs_norm = np.linalg.norm(text_embeddings_arr, axis=1).reshape(text_embeddings_arr.shape[0], 1)
+		docs_norm = np.where(docs_norm==0, 1, docs_norm)
+		unitvec_docs = text_embeddings_arr/docs_norm
+
+		return NumpyEmbeddingStorage(unitvec_docs)
 	
 
 if datasource == 'cranfield':
@@ -62,11 +99,11 @@ if datasource == 'cranfield':
 	doc_embeds = None
 	query_engine = None
 	query_vectors = {}
-	if cmdargs.vectorizer_type == 'word2vec':
+	if cmdargs.vectorizer_type in ['word2vec', 'tfidf']:
 		docs = []
 		for doc in cran_data:
 			docs.append(doc['W'])
-		vectorizer = Word2vecDocVectorizer()
+		vectorizer = Word2vecDocVectorizer() if cmdargs.vectorizer_type == 'word2vec' else TfIdfDocVectorizer(len(id2freq))
 		doc_embeds = vectorizer.make_doc_embedding_storage(docs)
 		query_engine = QueryEngineCore(doc_embeds, comparator_func=np.dot)
 		for query in cran_qrel:
@@ -79,11 +116,12 @@ if datasource == 'cranfield':
 			para2vec = np.load(f)
 		docs = []
 		for doc in cran_data:
-			docs.append(para2vec[doc2id['cran_{}'.format(doc['I'])]])
+			docs.append(para2vec[doc2id['cran_doc_{}'.format(doc['I'])]])
 		doc_embeds = NumpyEmbeddingStorage(np.array(docs))
 		def comparator(vec1, vec2):
 			sub = np.subtract(vec1,vec2)
 			return 15/np.dot(sub, sub)
+			#return float(np.dot(vec1, vec2)/np.linalg.norm(vec2))
 		query_engine = QueryEngineCore(doc_embeds, comparator_func = comparator)
 		for query in cran_qrel:
 			if len(cran_qrel[query]) == 0:
@@ -91,9 +129,13 @@ if datasource == 'cranfield':
 				continue
 			query_num = int(cran_qrel[query][0]['qnum'])
 			query_vectors[query] = para2vec[doc2id['cran_qry_{}'.format(query_num)]]
-			print(query, query_num)
+		#	print(query, query_num)
 
+	nr = dict()
 	for query in cran_qrel:
+		query_id = cran_qrel[query][0]['qnum']
+		nr[query_id] = []
+
 		doc_rels = {}
 		for d in cran_qrel[query]:
 			doc_rels[int(d['dnum'])] = int(d['rnum'])
@@ -105,6 +147,10 @@ if datasource == 'cranfield':
 		for result in results:
 			doc_id = int(cran_data[result[1]]['I'])
 			doc_rel = doc_rels[doc_id] if doc_id in doc_rels else 5
-			#print('{:d}\t{:d}\t{:d}\t{:0.16f}'.format(cran_qrel[query][0]['qnum'], doc_id, doc_rel, result[0]))
+			nr[query_id].append({'doc_id': doc_id, 'doc_rel': doc_rel, 'score': result[0]})
+			if cmdargs.tsv_output:
+				print('{:d}\t{:d}\t{:d}\t{:0.16f}'.format(query_id, doc_id, doc_rel, result[0]))
+	#with open('/dev/shm/tmp351.json', 'w') as f:
+	#	json.dump(nr, f, indent=4, sort_keys=True)
 
 
