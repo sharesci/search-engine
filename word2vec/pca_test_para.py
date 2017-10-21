a55c88f7ed4b9e5bffb6da4b1ec518a6f06e5d20
Perl Dev 1 <m.m.darcy@vikes.csuohio.edu>
2017-10-21 03:17:13 -0400

    Clean up paragraph2vec projection
      - Use TruncatedSVD instead of PCA
      - Add command-line arguments
      - Modularize the plotting and add labels/legends
      - Misc other cleanup

--- word2vec/pca_test_para.py
+++ word2vec/pca_test_para.py
@@ -1,48 +1,71 @@
 import os
 import pickle
+import json
+import re
 import numpy as np
+import time
+import gc
+import sys
 from matplotlib import pyplot as plt
-from sklearn.decomposition import PCA
-from cntk.ops.functions import load_model
+from sklearn.decomposition import PCA, TruncatedSVD
+import scipy.sparse
+from argparse import ArgumentParser
 
-test_words = ['http', 'html', 'neural', 'algorithm', 'math', 'science']
+
 
 def get_model_weights():
-	#z = load_model(os.path.join(os.getcwd(), "word2vec_checkpoint"))
-	#weights = z.E.value
 	with open('../largedata/paragraph_vectors.npy', 'rb') as f:
 		weights = np.load(f)
 	return weights
 
-def reduce_dimentions(weights):
-	reduced_weights = PCA(n_components = 2).fit_transform(weights)
-	return reduced_weights
 
-def plot_graph(words, all_points, token2id):
-	points = np.empty([len(words), 2])
-	points = all_points[:10]
-#	for idx, word in enumerate(words):
-#		word_id = token2id[word]
-#		points[idx] = all_points[word_id]
+def reduce_dimentions(weights):
+	return TruncatedSVD(n_components = 2).fit_transform(weights)
+
+
+def plot_graph(doc_names, all_points, doc2id, label='', annotate=False):
+	points = np.zeros([len(doc_names), 2])
+	for idx, doc in enumerate(doc_names):
+		doc_id = doc2id[doc]
+		points[idx] = all_points[doc_id]
 
 	#Plot the coordinates of test words
-	plt.scatter(points[:,0], points[:,1])
+	plt.scatter(points[:,0], points[:,1], label=label)
 
 	#Annotate the points
-	i = 0
-	for row in points:
-		plt.annotate(words[i+1], xy=row)
-		i += 1
+	if annotate:
+		i = 0
+		for row in points:
+			plt.annotate(doc_names[i], xy=row)
+			i += 1
 
+
+if __name__ == '__main__':
+	parser = ArgumentParser()
+	parser.add_argument('--annotate_points', dest='annotate_points', help='Annotate the data points', action='store_true', default=False)
+	parser.add_argument('--legend', dest='legend', help='Where to put the legend (compatible with pyplot syntax, "none" to have no legend)', action='store', default='none')
+	parser.add_argument('--doc2id_file', dest='doc2id_file', help='File where the doc2id JSON is located', action='store', default='../largedata/doc2id.json')
+	parser.add_argument('names_file', help='File from which to get the doc names to test, in JSON format', action='store')
+	cmdargs = parser.parse_args(sys.argv[1:])
+
+	weights = get_model_weights()
+	all_points = reduce_dimentions(weights)
+
+	with open(cmdargs.names_file, 'r') as f:
+		test_doc_names = json.load(f)
+
+	with open(cmdargs.doc2id_file, 'r') as f:
+		doc2id = json.load(f)
+
+	fig = plt.figure(figsize=(6.5,5.5))
+
+	for k in sorted(test_doc_names):
+		plot_graph(test_doc_names[k], all_points, doc2id, label=k, annotate=cmdargs.annotate_points)
+
+	if cmdargs.legend != 'none':
+		plt.legend(loc=cmdargs.legend)
+
+	fig.tight_layout()
 	plt.show()
-
-weights = get_model_weights()
-all_points = reduce_dimentions(weights)
-
-training_data = pickle.load(open('../largedata/text_training_data.pickle', 'rb'))
-inverse_dict = {v: k for k,v in training_data.doc2id.items()}
-
-plot_graph(inverse_dict, all_points, training_data.doc2id)
-
 
 
