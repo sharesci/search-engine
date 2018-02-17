22037b279a76dc76a9471f7dfceaeb789496625b
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2018-02-17 16:49:12 -0500

    Integrate doc recommendation system with user history

--- word2vec/VectorizerDocSearchEngine.py
+++ word2vec/VectorizerDocSearchEngine.py
@@ -7,6 +7,8 @@ import sklearn.svm
 
 import pymongo
 
+import itertools
+
 from QueryEngineCore import QueryEngineCore
 from DocVectorizer import Word2vecDocVectorizer, TfIdfDocVectorizer
 
@@ -28,7 +30,7 @@ class VectorizerDocSearchEngine:
 		docs = []
 		self._idx2id = []
 		self._id2idx = dict()
-		for mongo_doc in self._mongo_db['cranfield'].find({}):
+		for mongo_doc in self._mongo_db['papers'].find({}):
 			# ID is mapped to the current index in the `docs` array
 			# Obviously, it's important to do this before appending
 			# to `docs`.
@@ -62,11 +64,43 @@ class VectorizerDocSearchEngine:
 		if max_results == 0:
 			max_results = sys.maxsize
 
-		user_history_vec = np.random.randint(low=0, high=2, size=len(self._doc_embeds))
-		user_svm = sklearn.svm.LinearSVC(class_weight='balanced', verbose=False, max_iter = 2000, C=0.1)
+		# Init the history vector for the user
+		user_history_vec = np.zeros(len(self._doc_embeds))
+
+		# Get user history
+		cursor = self._mongo_db['users'].find({'_id': user_id}, {'_id': 1, 'docIds': 1}).limit(1)
+		if cursor.count() == 0:
+			return []
+
+		# Convert user history to vector
+		# Note: we make and store the array for doc indexes to use them
+		# again later
+		user_docIds_arr = cursor[0]['docIds']
+		user_docIdxs = set()
+		for docId in user_docIds_arr:
+			if docId not in self._id2idx:
+				continue
+			user_docIdxs.add(self._id2idx[docId])
+		for docIdx in user_docIdxs:
+			user_history_vec[docIdx] = 1
+
+		# If there weren't any example docs, the SVM can't train, so
+		# just fail and return empty
+		if len(user_docIdxs) == 0:
+			return []
+
+		# Train the SVM
+		# TODO: Move this process offline so we don't have to train from scratch on every query
+		user_svm = sklearn.svm.LinearSVC(class_weight='balanced', verbose=False, max_iter = 2000, C=0.1, tol=1e-5)
 		user_svm.fit(self._doc_embeds.as_numpy_array(), user_history_vec)
 
-		results = QueryEngineCore.search_static(np.zeros(1), self._doc_embeds, lambda vec, qvec: float(user_svm.decision_function([vec])), max_results=(offset+max_results))[offset:(offset+max_results)]
+		results = QueryEngineCore.search_static(np.zeros(1), self._doc_embeds, lambda vec, qvec: float(user_svm.decision_function([vec])), max_results=(offset+max_results+len(user_docIds_arr)))
+
+		# Filter to exclude any docs the user has already seen
+		results[:] = itertools.filterfalse((lambda x: x[1] in user_docIdxs), results)
+
+		# Get the specified result window
+		results = results[offset:offset+max_results]
 
 		return self._convert_result_format(results)
 		
