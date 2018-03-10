3a6777dd2d171e2cfea63d1f9d816afc3c986a3a
Nick McCoy <nmccoy82@gmail.com>
2018-03-10 16:51:54 -0500

    Changed bigram_indexer for wiki docs

--- bigram_indexer.py
+++ bigram_indexer.py
@@ -67,7 +67,7 @@ def get_database_size():
     cur = CONN.cursor()
     size = sys.maxsize
     try:
-        cur.execute("select pg_database_size('sharesci')")
+        cur.execute("select pg_database_size('test')")
         size = cur.fetchall()[0][0]
     except psycopg2.Error as error:
         print('Failed to get database size', file=sys.stderr)
@@ -119,6 +119,7 @@ def get_doc_ids(text_ids):
 #
 def populate_document_table(text_ids, doc_lengths, options):
     excluded_docs = set([])
+    doc_type = options.doc_type
 
     if options.get_parent_docs:
         parent_table = np.array([text_id.split('_')[-1] for text_id in text_ids])
@@ -135,7 +136,7 @@ def populate_document_table(text_ids, doc_lengths, options):
         doc_table = [(text_ids[i],
                       doc_lengths[i][0],
                       None,
-                      1
+                      doc_type
                      ) for i in range(len(text_ids)) if i not in excluded_docs]
 
     sql = """INSERT INTO document (text_id, length, parent_doc, type)
@@ -189,6 +190,20 @@ def populate_tables(raw_tf, text_ids, terms, options):
     while num_inserted < bigram_length:
         cursor = CONN.cursor()
         try:
+            #print("    Calling insert_term")
+            cursor.callproc('insert_term', [bigram_terms[num_inserted:(num_inserted+bigram_batch_size)]])
+            #data = cursor.fetchone()
+            #if data:
+            #    gram_ids += data[0]
+            #else:
+            #    print("Warning: insert_bigram_df function returned null.")
+        except psycopg2.Error as error:
+            print(error)
+        CONN.commit()
+#        cursor.close()
+
+        try:
+            #print("    Calling insert_bigram_df");
             cursor.callproc('insert_bigram_df', [bigram_terms[num_inserted:(num_inserted+bigram_batch_size)], df_values[num_inserted:(num_inserted+bigram_batch_size)]])
             data = cursor.fetchone()
             if data:
@@ -199,10 +214,12 @@ def populate_tables(raw_tf, text_ids, terms, options):
             print(error)
         CONN.commit()
         cursor.close()
+
         num_inserted += bigram_batch_size
         if bigram_length - num_inserted > 0 and (num_inserted/1000000).is_integer():
             print("{} bigrams remaining.".format(bigram_length - num_inserted))
-    print("Data Inserted")
+    print("Data Inserted into term, gram, and idf")
+
     df_values = None
     bigram_terms = None
 
@@ -218,11 +235,22 @@ def populate_tables(raw_tf, text_ids, terms, options):
                 ])
 
     print("Inserting {} rows into tf table".format(len(tf_values)))
-    sql = """INSERT INTO tf(gram_id, doc_id, lnc)
+
+    num_inserted = 0
+    batch_size = 100000
+    total_tf_values = len(tf_values)
+
+    for chunks in [tf_values[x:x+batch_size] for x in range(0, total_tf_values, batch_size)]:
+        sql = """INSERT INTO tf(gram_id, doc_id, lnc)
              VALUES %s
-             ON CONFLICT (gram_id, doc_id) DO UPDATE 
+             ON CONFLICT (gram_id, doc_id) DO UPDATE
                 SET lnc=EXCLUDED.lnc"""
-    insert(sql, tf_values)
+        insert(sql, chunks)
+        print("Commiting data - {} inserted".format(num_inserted))
+        num_inserted += len(chunks)
+        if total_tf_values - num_inserted > 0 and (num_inserted/1000000).is_integer():
+            print("{} tf values remaining.".format(total_tf_values - num_inserted))
+
     print("Data Inserted.")
 
 
@@ -237,22 +265,38 @@ def load_files(root, mappings):
     """
     token_dict = {}
 
-    for subdir, _, tar_files in os.walk(root):
-        print("Processing Files\n")
-        for tar_file in tar_files:
-            if tar_file.endswith(".tar.gz"):
-                tar_file_path = subdir + os.path.sep + tar_file
-                tar = tarfile.open(tar_file_path)
-                for member in tar.getmembers():
-                    file = tar.extractfile(member)
-                    if file is not None: #only read regular files
-                        doc_id = re.sub(r'.preproc$', '', os.path.basename(member.name))
-                        if doc_id in mappings[0]:
-                            doc_id = mappings[1][mappings[0].index(doc_id)]
-                        print("Processing {0}".format(member.name), end="\r")
-                        text = file.read().decode("utf-8")
-                        text = text.translate(str.maketrans('', '', string.punctuation))
-                        token_dict[doc_id] = text
+    # for subdir, _, tar_files in os.walk(root):
+    #     print("Processing Files\n")
+    #     for tar_file in tar_files:
+    #         if tar_file.endswith(".tar.gz"):
+    #             tar_file_path = subdir + os.path.sep + tar_file
+    #             tar = tarfile.open(tar_file_path)
+    #             for member in tar.getmembers():
+    #                 file = tar.extractfile(member)
+    #                 if file is not None: #only read regular files
+    #                     doc_id = re.sub(r'.preproc$', '', os.path.basename(member.name))
+    #                     if doc_id in mappings[0]:
+    #                         doc_id = mappings[1][mappings[0].index(doc_id)]
+    #                     print("Processing {0}".format(member.name), end="\r")
+    #                     text = file.read().decode("utf-8")
+    #                     text = text.translate(str.maketrans('', '', string.punctuation))
+    #                     token_dict[doc_id] = text
+    print("Processing files in {0}...".format(root))
+    count = 0
+
+    for dirpath, dnames, fnames in os.walk(root):
+        for file in fnames:
+            count = count + 1
+            doc_id = file  # re.sub(r'.preproc$', '', os.path.basename(member.name))
+
+            if doc_id in mappings:
+                external_id = mappings[doc_id]
+            file_obj = open(os.path.join(dirpath, file), "r", encoding="utf-8")
+            text = file_obj.read()  #.decode("utf-8")
+            text = text.translate(str.maketrans('', '', string.punctuation))
+            token_dict[external_id] = text
+
+        print("    found {0} files".format(count), end="\r")
         print("Processing Complete.")
 
     return token_dict
@@ -260,9 +304,9 @@ def load_files(root, mappings):
 
 def index_terms(token_dict, options):
     print("Calculating raw tf values.")
-    #vectorizer = CountVectorizer(token_pattern=r'(?u)\b\w[A-Za-z_-]{1,19}\b', ngram_range=(1, 2))
-    #vectorizer = CountVectorizer(tokenizer=lambda x: [token for token in nltk.word_tokenize(x)], ngram_range=(1, 2))
-    vectorizer = CountVectorizer(tokenizer=lambda x: [stemmer.stem(token) for token in nltk.word_tokenize(x)], ngram_range=(1, 2))
+    #vectorizer = CountVectorizer(token_pattern=r'(?u)\b\w[A-Za-z_-]{1,19}\b', ngram_range=(1, 1))
+    #vectorizer = CountVectorizer(tokenizer=lambda x: [token for token in nltk.word_tokenize(x)], ngram_range=(1, 1))
+    vectorizer = CountVectorizer(tokenizer=lambda x: [stemmer.stem(token) for token in nltk.word_tokenize(x)], ngram_range=(1, 1))
     raw_tf = vectorizer.fit_transform(token_dict.values())
     print("Calculation of raw tf values complete.")
     terms = vectorizer.get_feature_names()
@@ -280,6 +324,7 @@ if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-d", dest="doc_dir")
     parser.add_option("-m", dest="mapping_file")
+    parser.add_option("--type", default=1, dest="doc_type")
     parser.add_option("--new-docs", action="store_true", default=False, dest="new_docs")
     parser.add_option("--get-parent-docs",
                       default=False,
@@ -305,17 +350,16 @@ if __name__ == "__main__":
 
     #if OPTIONS.doc_dir and OPTIONS.mapping_file:
     if OPTIONS.mapping_file:
-        mappings = [[], []]
+        mappings = dict()
         with open(OPTIONS.mapping_file) as f:
             for m in csv.reader(f):
-                mongoID = m[0]
-                if mongoID == "_id": continue
-                else: mongoID = m[0][9:-1]
+                externalID = m[0]
+                if externalID == "_id": continue
+                else: externalID = m[0][9:-1]
 
                 docID = m[1]
+                mappings[docID] = externalID
 
-                mappings[0].append(mongoID)
-                mappings[1].append(docID)
                 # if 'arXiv_id' in m:
                 #     mappings[0].append(m["_id"])
                 #     mappings[1].append(m["arXiv_id"])
@@ -329,3 +373,4 @@ if __name__ == "__main__":
         print("All done.")
     else:
         print("Please specify path to the folder which contains all .tar.gz")
+
