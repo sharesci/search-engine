8b23b507a8113d33474f59d3745b74a62389767b
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2018-04-26 12:34:26 -0400

    Implement query caching

--- word2vec/VectorizerDocSearchEngine.py
+++ word2vec/VectorizerDocSearchEngine.py
@@ -6,6 +6,7 @@ import os
 import sklearn.svm
 import time
 import heapq
+import re
 
 import pymongo
 import bson.objectid
@@ -46,7 +47,73 @@ class VectorizerDocSearchEngine:
 
 		self._inverted_index = MongoInvertedIndex(self._mongo_db['papers_index'], self._mongo_db['special_objects'], self._mongo_db['papers_vecs'], self._tfidf_vec_field_name)
 
+		self._query_engine = TfIdfQueryEngineCore(self._inverted_index)
+
+		self._cache = dict()
+		self._max_cache_age = 7200   # seconds
+		self._cache_access_counter = 0
+
 		self._reload_all_docs()
+
+
+	def _garbage_collect_caches(self):
+		cur_time = time.time()
+		for main_key in self._cache:
+			to_remove = []
+			for entry in self._cache[main_key]:
+				if entry['expire_time'] < cur_time:
+					to_remove.append(entry)
+			# Shortcut when removing all entries
+			if len(to_remove) == len(self._cache[main_key]):
+				del self._cache[main_key]
+				continue
+
+			for entry in to_remove:
+				self._cache[main_key].remove(entry)
+
+	
+	## Returns a result set if a cached one is found for the given
+	# parameters. If there is not a cache hit, returns None.
+	#
+	def _get_results_from_cache(self, main_key, extra_keys):
+		if main_key not in self._cache:
+			return None
+
+		cache_entries = self._cache[main_key]
+		for entry in cache_entries:
+			# We intentionally only check against the given extra
+			# keys here, even if the cache entry itself had more
+			# extra keys
+			is_match = True
+			for key in extra_keys:
+				if key not in entry['extra_keys'] or entry['extra_keys'][key] != extra_keys[key]:
+					is_match = False
+					break
+			if not is_match:
+				continue
+
+			if entry['expire_time'] <= time.time():
+				cache_entries.remove(entry)
+				break
+
+			return entry['value']
+
+		return None
+
+
+	def _add_to_cache(self, main_key, extra_keys, value):
+		self._cache_access_counter += 1
+		if 20 < self._cache_access_counter:
+			self._garbage_collect_caches()
+
+		if main_key not in self._cache:
+			self._cache[main_key] = []
+
+		self._cache[main_key].append({
+			'expire_time': time.time() + self._max_cache_age,
+			'extra_keys': extra_keys,
+			'value': value
+		});
 
 
 	def _vec_to_sparse_tuples(self, vec):
@@ -85,12 +152,26 @@ class VectorizerDocSearchEngine:
 			{('other.' + self._tfidf_vec_field_name): {'$exists': False}},
 			{('other.' + self._paragraph_vec_field_name): {'$exists': False}}
 		]}
-		for mongo_doc in self._mongo_db['papers'].find(doc_query, projection={'_id': True, 'abstract': True, 'body': True, ('other.' + self._tfidf_vec_field_name): True, ('other.' + self._paragraph_vec_field_name): True}):
+		doc_projection = {'_id': True,
+			'abstract': True,
+			'body': True,
+			('other.' + self._tfidf_vec_field_name): True,
+			('other.' + self._paragraph_vec_field_name): True
+		}
+		mongo_cursor = self._mongo_db['papers'].find(doc_query, projection=doc_projection)
+		mongo_cursor.batch_size(400);
+		for mongo_doc in mongo_cursor:
 
 			if 'other' not in mongo_doc or self._tfidf_vec_field_name not in mongo_doc['other']:
 				text_field = 'body' if 'body' in mongo_doc else 'abstract'
 				raw_embed = self._tfidf_vectorizer.make_doc_vector(mongo_doc[text_field]).astype(float)
-				norm_embed = self._vec_to_sparse_tuples(raw_embed / np.sum(np.square(raw_embed)))
+				embed_len = np.sum(np.square(raw_embed))
+
+				# Prevent divide-by-zero errors
+				if embed_len == 0:
+					embed_len = 1
+
+				norm_embed = self._vec_to_sparse_tuples(raw_embed / embed_len)
 
 				new_tfidf_docs.append((mongo_doc['_id'], norm_embed))
 				num_newly_vectorized['tfidf'] += 1
@@ -103,14 +184,12 @@ class VectorizerDocSearchEngine:
 			if 900 < len(new_para2vec_docs):
 				self._flush_new_para2vec_docs(new_para2vec_docs)
 				new_para2vec_docs = []
-			if 2000 < len(new_tfidf_docs):
+			if 20000 <= len(new_tfidf_docs):
 				self._flush_new_tfidf_docs(new_tfidf_docs)
 				new_tfidf_docs = []
 
 		self._flush_new_para2vec_docs(new_para2vec_docs)
 		self._flush_new_tfidf_docs(new_tfidf_docs)
-
-		self._query_engine = TfIdfQueryEngineCore(self._inverted_index)
 
 		print('Reloaded: {} new paragraph vectors, {} new TF-IDFs. Total time: {:0.3f} seconds'.format(num_newly_vectorized['paragraph2vec'], num_newly_vectorized['tfidf'], time.perf_counter() - start_time))
 
@@ -123,14 +202,36 @@ class VectorizerDocSearchEngine:
 
 
 	def search_qs(self, query, **generic_search_kwargs):
+		query = re.sub(r"\s+", " ", query.lower())
+
+		cache_extra_keys = {'max_results': 10, 'offset': 0}
+		if 'max_results' in generic_search_kwargs:
+			cache_extra_keys['max_results'] = generic_search_kwargs['max_results']
+		if 'offset' in generic_search_kwargs:
+			cache_extra_keys['offset'] = generic_search_kwargs['offset']
+		main_key = 'search_qs.' + query
+		cached_result = self._get_results_from_cache(main_key, cache_extra_keys)
+		if cached_result is not None:
+			return cached_result
+
 		query_vec = self._tfidf_vectorizer.make_doc_vector(query);
 		query_vec = self._vec_to_sparse_tuples(query_vec)
-		return self.search_queryvec(query_vec, **generic_search_kwargs)
+		results = self.search_queryvec(query_vec, **generic_search_kwargs)
+
+		self._add_to_cache(main_key, cache_extra_keys, results)
+
+		return results
 
 
 	def search_docid(self, doc_id, max_results=20, offset=0, **generic_search_kwargs):
 		if not isinstance(doc_id, bson.objectid.ObjectId):
 			doc_id = bson.objectid.ObjectId(doc_id)
+
+		cache_extra_keys = {'max_results': max_results, 'offset': offset}
+		main_key = 'search_docid.' + str(doc_id)
+		cached_result = self._get_results_from_cache(main_key, cache_extra_keys)
+		if cached_result is not None:
+			return cached_result
 
 		# Start with a TF-IDF search to get a rough set of candidates
 		qs_max_results = max_results * 2 + offset
@@ -164,12 +265,21 @@ class VectorizerDocSearchEngine:
 		if len(final_results) < offset+1:
 			return []
 
-		return final_results[offset:offset+max_results]
+		results = final_results[offset:offset+max_results]
+		self._add_to_cache(main_key, cache_extra_keys, results)
+
+		return results
 
 
 	def search_userid(self, user_id, max_results=sys.maxsize, offset=0, getFullDocs=False, history_window=100):
 		if max_results == 0:
 			max_results = sys.maxsize
+
+		cache_extra_keys = {'max_results': max_results, 'offset': offset}
+		main_key = 'search_userid.' + str(user_id)
+		cached_result = self._get_results_from_cache(main_key, cache_extra_keys)
+		if cached_result is not None:
+			return cached_result
 
 		# Get user history
 		user_blob = self._mongo_db['users'].find_one({'_id': user_id}, {'_id': 1, 'docIds': 1, 'terms': 1})
@@ -186,7 +296,7 @@ class VectorizerDocSearchEngine:
 		if 'terms' in user_blob and len(user_blob['terms']) != 0:
 			recent_terms = ' '.join(user_blob['terms'][:history_window])
 			qs_max_results = max_results * 2 + offset
-			candidate_docs = self.search_qs(recent_terms, offset=0, max_results=qs_max_results, certainty_factor=0.7)
+			candidate_docs = self.search_qs(recent_terms, offset=0, max_results=qs_max_results, certainty_factor=0.05)
 
 			# We would need doc IDs to train the SVM. If we don't
 			# have any, just return the candidates
@@ -281,7 +391,10 @@ class VectorizerDocSearchEngine:
 		results = results[offset:offset+max_results]
 
 		# Convert the doc IDs into strings
-		return [(score, str(doc_id)) for score, doc_id in results]
+		results = [(score, str(doc_id)) for score, doc_id in results]
+		self._add_to_cache(main_key, cache_extra_keys, results)
+
+		return results
 
 
 	def search_queryvec(self, query_vec, max_results=sys.maxsize, offset=0, getFullDocs=False, **kwargs):
